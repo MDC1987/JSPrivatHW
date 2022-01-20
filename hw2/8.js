efc8b4f5cb42c1cb3eae350ba8edaa419451af7b
Anton Kondratenko <kondra22@gmail.com>
2022-01-21 00:17:32 +0200

    hw2

--- hw2/8.js
+++ hw2/8.js
@@ -1,16 +1,16 @@
 var i = 0;
 
 function f (arr) {
-    if (Array.isArray(arr)) {
+    if (arr.length === 0) {
+        throw new Error ('parameter can not be an empty');
+    }
+    else if (Array.isArray(arr)) {
         console.log (arr[i]);
         i++;
         if (i < arr.length) 
             f (arr);
     }    
-    else if (arr.length == 0) {
-    //    console.log (arr.length);
-        throw new Error ('parameter can not be an empty');
-    } else {
+    else {
         throw new Error ('parameter type should be an array')
     }
 }
