c80d40f0b842ce7a1a4b48d3cd95a0c72d2bb34a
Anton Kondratenko <kondra22@gmail.com>
2022-01-21 07:12:35 +0200

    hw2

--- hw2/8.js
+++ hw2/8.js
@@ -1,9 +1,11 @@
 var i = 0;
 
 function f (arr) {
+    // проверка на отсутствие элементов массива
     if (arr.length === 0) {
         throw new Error ('parameter can not be an empty');
     }
+    // проверка на передачу массива чисел и вывода его элементов построчно
     else if (Array.isArray(arr)) {
         console.log (arr[i]);
         i++;
@@ -11,9 +13,9 @@ function f (arr) {
             f (arr);
     }    
     else {
-        throw new Error ('parameter type should be an array')
+        throw new Error ('parameter type should be an array');
     }
-}
+};
 
 f([1,2,3]);
 // 1
